b70c420615634bd5b3bf258e3fe98a7fc8f963c7
Kylie Stanley <knstanley11@gmail.com>
2019-01-06 20:57:38 -0700

    Updates

---
+++ precache-manifest.a7ee53dc15ca30e0af9aa8455e500cbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5a723b953ab703507c1",
+    "url": "/movie-tracker/static/js/runtime~main.f5a723b9.js"
+  },
+  {
+    "revision": "0c13a5e99ad026bc4b9a",
+    "url": "/movie-tracker/static/js/main.0c13a5e9.chunk.js"
+  },
+  {
+    "revision": "73c631e40486b09c0ddc",
+    "url": "/movie-tracker/static/js/1.73c631e4.chunk.js"
+  },
+  {
+    "revision": "0c13a5e99ad026bc4b9a",
+    "url": "/movie-tracker/static/css/main.445fa052.chunk.css"
+  },
+  {
+    "revision": "e15ca9d3eadde04f0cfd5deaf410a14f",
+    "url": "/movie-tracker/index.html"
+  }
+];
